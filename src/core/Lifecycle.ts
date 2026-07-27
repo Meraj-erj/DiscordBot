668d2ffd2c4be604425cdead95d56343252284f5
DevOps <108233858+Meraj-erj@users.noreply.github.com>
2026-07-28 03:01:17 +0330

    feat: add application lifecycle foundation

---
+++ src/core/Lifecycle.ts
@@ -0,0 +1,9 @@
+export class Lifecycle {
+    public async start(): Promise<void> {
+        // Application startup lifecycle
+    }
+
+    public async shutdown(): Promise<void> {
+        // Application shutdown lifecycle
+    }
+}
